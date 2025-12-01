c9e0a5404b8c3c6b86eeef2ebc6658b092c09a27
Jonathan Ferguson <jono@furrygarcon.com>
2025-12-02 09:51:18 +1300

    assorted tidying
    prefer Option over Some
    we appear to be using logback rather than log4j
    we don't have the expected assets for google fonts
    classpath/json.js is a punt - likely wrong.

--- src/main/scala/metl/model/ConfigFileReader.scala
+++ src/main/scala/metl/model/ConfigFileReader.scala
@@ -8,27 +8,27 @@ trait ConfigFileReader extends Logger {
 	protected def getImmediateNodes(e:Node,tagName:String):List[Node] = (e \ tagName).theSeq.toList
 	protected def getChildrenNodes(e:Node):List[Node] = e.child.toList
 	protected def getNodeString(e:Node,tagName:String):Option[String] = try {
-		Some(getNodes(e,tagName).map(_.toString).mkString(""))
+		Option(getNodes(e,tagName).map(_.toString).mkString(""))
 	} catch {
 		case e:Throwable => None
 	}
 	protected def getAttr(e:Node,attrName:String):Option[String] = (e \ "@%s".format(attrName)).text.toString match {
-		case t:String if t.length > 0 => Some(t)
+		case t:String if t.length > 0 => Option(t)
 		case _ => None.asInstanceOf[Option[String]]
 	}
 	protected def getText(e:Node,tag:String):Option[String] = (e \ tag).text.toString match {
-		case t:String if t.length > 0 => Some(t)
+		case t:String if t.length > 0 => Option(t)
 		case _ => None.asInstanceOf[Option[String]]
 	}
 	protected def getBool(e:Node,tag:String):Option[Boolean] = getText(e,tag) match {
-		case Some(t) if t.isInstanceOf[String] && t.toLowerCase.trim == "true" => Some(true)
-		case Some(t) if t.isInstanceOf[String] && t.toLowerCase.trim == "false" => Some(false)
+		case Some(t) if t.isInstanceOf[String] && t.toLowerCase.trim == "true" => Option(true)
+		case Some(t) if t.isInstanceOf[String] && t.toLowerCase.trim == "false" => Option(false)
 		case _ => None
 	}
 	protected def getInt(e:Node,tag:String):Option[Int] = getText(e,tag) match {
 		case Some(t) if t.isInstanceOf[String] && t.length > 0 => {
 			try {
-				Some(t.toInt)
+				Option(t.toInt)
 			} catch {
 				case e:Throwable => None
 			}
@@ -38,7 +38,7 @@ trait ConfigFileReader extends Logger {
 	protected def getDouble(e:Node,tag:String):Option[Double] = getText(e,tag) match {
 		case Some(t) if t.isInstanceOf[String] && t.length > 0 => {
 			try {
-				Some(t.toDouble)
+				Option(t.toDouble)
 			} catch {
 				case e:Throwable => None
 			}
@@ -48,7 +48,7 @@ trait ConfigFileReader extends Logger {
 	protected def getLong(e:Node,tag:String):Option[Long] = getText(e,tag) match {
 		case Some(t) if t.isInstanceOf[String] && t.length > 0 => {
 			try {
-				Some(t.toLong)
+				Option(t.toLong)
 			} catch {
 				case e:Throwable => None
 			}
