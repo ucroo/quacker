e9f20b722cd7fcc75493b50830176201f0650d9b
Alex Goncalves <schroedergoncalves@gmail.com>
2025-12-17 19:05:16 -0500

    fix: don't remove all docker images on local boot

--- local.sh
+++ local.sh
@@ -1,9 +1,13 @@
 echo "please ensure that you've added the config to the quacker, to enable appropriate checks, by putting the xml files into the config directory"
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-sbt.sh compile
-sbt.sh package
+
+./sbt.sh compile
+./sbt.sh package
 mkdir -p webapps
 cp target/scala-2.11/*.war webapps/quacker.war
+
+if docker ps -a --format '{{.Names}}' | grep -q '^quacker$'; then
+    docker rm -f quacker
+fi
+
 docker build -t quacker .
-docker run -d -p 443:443 -it quacker
+docker run -d -p 443:443 --name quacker -it quacker 
